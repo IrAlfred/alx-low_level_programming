0cb7f2005d29a36d879a71b7179366055a2f9d01
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-18 05:50:14 -0800

    First test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i = 0;
+	while(i < 25)
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+}
