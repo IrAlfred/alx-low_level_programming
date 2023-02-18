e1ef04a9fd3b8d9bf88614b18f4ccf313eed9c8a
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-18 05:01:45 -0800

    Add missing stdio.h lib

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
 /**
  * main - this program assign a random number and prints if it is positive, negative or zero
