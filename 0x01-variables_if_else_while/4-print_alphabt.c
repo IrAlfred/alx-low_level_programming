c2f662237a1e52718b209ee6e58a114630761d05
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-18 06:25:51 -0800

    First Try

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet except q and e
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i = 0;
+	while (i < 26)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c++);
+		}
+		i = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
