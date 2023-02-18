5f08523185ecad88e1e480a20eaa489adf73d19e
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-18 07:30:59 -0800

    Combinations print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program that print combinations of two dgit
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int ch1;
+	int ch2;
+
+	ch1='0';
+	while (ch1 <= '9')
+	{
+		ch2 = ch1 + 1;
+		while (ch2 <= 9)
+		{
+			putchar(ch1);
+			putchar(ch2);
+			if (ch1 != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			ch2++;
+		}
+		ch1++;
+	}
+	putchar('\n');
+	return (0);
+}
