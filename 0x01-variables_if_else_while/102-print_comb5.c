bfa5ab38537139b11c335a8bcd6d73c80439adf2
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-19 13:18:09 -0800

    combinasons 5 values

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - This is the enty point of the program. It's about combinations
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (j != i)
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
