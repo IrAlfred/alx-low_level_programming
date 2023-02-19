e5ae83d0858dcbd5e7ce4f290b444d44a1cfb67a
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-19 13:07:08 -0800

    First try with combinaisons

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - This program is about combinations
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int c;
+	int b;
+	int d;
+	
+	c = '0';
+	while (c <= '9')
+	{
+		b = c + 1;
+		while (b <= '9')
+		{
+			d = b + 1;
+			while (d <= '9')
+			{
+				putchar(c);
+				putchar(b);
+				putchar(d);
+				if (c != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				d++;
+			}
+			b++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
