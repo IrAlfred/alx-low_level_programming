8613c5c6097266e1c30012545df43742bf8321cd
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-22 12:43:55 -0800

    First test

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - Prints a sign of a number passed as argument
+ * @n: number to test
+ * Retur: 1 (number > 0) 0(zero) -1 (number <0)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		 return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
