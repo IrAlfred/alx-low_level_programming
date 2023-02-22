55519877a7729351d093884e4650b1341fc49ce0
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-22 13:21:33 -0800

    First test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - Prints the last digit
+ * @n: argument
+ * Return: int value
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	return (last);
+}
