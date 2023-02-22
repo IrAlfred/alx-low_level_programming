010642fddd88b41a5eaff9478dcd368f02834944
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-22 13:37:30 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 23)
+	{
+		while (j <=59)
+		{
+			putchar((i / 10) + 48);
+			putchar((1 % 10) + 48);
+			putchar(':');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			putchar('\n');
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+}
