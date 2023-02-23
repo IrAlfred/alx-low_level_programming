096e6513058dc543f4ed1566db4f4e1c1ad1519c
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-23 04:08:42 -0800

    First upload

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * sum - sum of multiple of 3 or 5 below 1024
+ * Return: void
+ */
+void sum(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i <= 1024)
+	{
+		if (i % 3 == 0 || i % 5 ==0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
