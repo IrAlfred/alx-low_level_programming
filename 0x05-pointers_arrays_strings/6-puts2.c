07ed926e9a1bc9e0670000e8e5c4cc2f8763bffb
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-02-28 04:46:10 -0800

    Print every odd char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints the odd character of a string
+ * @s: string input
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+			i++;
+		}
+	}
+}
