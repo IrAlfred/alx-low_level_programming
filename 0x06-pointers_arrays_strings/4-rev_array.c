3c1a9c6418ec5ba43486776a97a99cec0bc0b517
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-03-01 07:44:23 -0800

    Add string_toupper function

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of a array
+ * @a: array
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tmp;
+
+	while (i < n/2)
+	{
+		tmp = *(a + i);
+		*(a + i) = *(a + (n - i - 1));
+		*(a + (n - i - 1)) = tmp;
+		i++;
+	}
+}
