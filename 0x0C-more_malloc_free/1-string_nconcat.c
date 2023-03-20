21160122d0bd0165cc73e52ca4eb24a8af60c9c1
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-03-20 07:51:41 -0700

    Correct case of null values

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int length1 = 0, length2 = 0, i, j;
 	char *new_str;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[length1] != '\0')
 		length1++;
