127e738710edab7de7aab7045b44a910383d109f
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-03-20 07:48:23 -0700

    Concatenation of strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of characters to concatenates from s2
+ *
+ * Return: pointer to the new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int length1 = 0, length2 = 0, i, j;
+	char *new_str;
+
+	while (s1[length1] != '\0')
+		length1++;
+	while (*(s2 + length2) != '\0')
+		length2++;
+	if (n > length2)
+		n = length2;
+	new_str = malloc(sizeof(char) * (length1 + n));
+	if (new_str == NULL)
+		return (NULL);
+	for (i = 0; i < length1; i++)
+	{
+		*(new_str + i) = s1[i];
+	}
+
+	for (j = 0; j < n; j++, i++)
+	{
+		*(new_str + i) = *(s2 + j);
+	}
+	*(new_str + i) = '\0';
+	return (new_str);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,4 @@
 
 void _putchar(char);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
