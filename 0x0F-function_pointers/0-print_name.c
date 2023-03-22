940ed618cec91980332c94bac2b4e61ca68264e2
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-03-22 13:15:52 -0700

    Print name funtion

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints the name passed as argument
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+void _putchar(char);
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+#endif /* end of _FUNCTION_POINTERS_H_*/
