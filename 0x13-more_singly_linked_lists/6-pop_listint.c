c5d4262e6e28879ef1aa77c647423612d5815fd3
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-05 13:25:21 -0700

    Pop list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,11 +11,12 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int data;
 
-	if (*head)
+	if (*head == NULL)
 		return (0);
 	tmp = *head;
-	data = tmp->n;
+	data = (*head)->n;
 	*head = (*head)->next;
 	free(tmp);
+
 	return (data);
 }
