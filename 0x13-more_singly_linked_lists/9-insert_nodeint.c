67dbd725e9cd3e2408f3f8b382db8bd637a67423
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-05 14:51:41 -0700

    Insert node in middle of linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Insert a node at a given pos
+ * @head: head of linked list
+ * @idx: index where node should be added
+ * @n: data to of the node
+ *
+ * Return: new list
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *tmp;
+	unsigned int i = 0;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
+	tmp = *head;
+	while (tmp &&  i < idx)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->n = n;
+	node->next = tmp->next;
+	tmp->next = node;
+
+	return (node);
+}
