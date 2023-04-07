f0adf7ab6166c8f564b6965063443ceeb715d0c9
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-07 04:30:05 -0700

    Come back from 63 to 31

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int i = 0;
 	int j;
 
-	for (j = 63; j >= 0; j--)
+	for (j = 31; j >= 0; j--)
 	{
 		if ((n & (1 << j)) != (m & (1 << j)))
 			i++;
