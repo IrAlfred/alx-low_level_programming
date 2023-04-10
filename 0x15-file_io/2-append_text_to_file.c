6cd30c79ed27ffb4827bc94eb686443290629ba3
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 23:29:58 -0700

    Some new test

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
+	int nbLetters = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_TRUNC, 0600);
+
+	if (text_content == NULL)
+	{
+		if (fd == -1)
+			return (1);
+		else
+			return (-1);
+	}
+
+	while (text_content[nbLetters])			
+		nbLetters++;
+	if (write(fd, text_content, nbLetters) == -1)
+		return (-1);
+	return (1);
 }
