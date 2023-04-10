feaeb32e7cc028e7aae50e5a4df2c10fecc9065b
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 22:11:14 -0700

    Some corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content)
 	
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT, 0600);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
 		close(fd);
-		return (fd);
+		return (1);
 	}
 
 	fd = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
@@ -32,6 +32,6 @@ int create_file(const char *filename, char *text_content)
 	if(wrCount != sizeof(text_content))
 		return (-1);
 
-	close(1);
-	
+	close(fd);
+	return (1);
 }
