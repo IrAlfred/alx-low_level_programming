bfaa6531540f46ebef8745013bc1457c5e6db336
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 01:03:38 -0700

    Prototypes

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: number of arguments
+ * @argv: array of strings
+ *
+ * Return: int
+ */
+int void main(int argc, char **argv)
+{
+	
+}
