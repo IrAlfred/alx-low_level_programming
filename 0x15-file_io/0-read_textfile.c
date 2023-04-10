02a54104a9cc2ca5ac1de225341c3074a83c12a0
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 20:43:23 -0700

    Close buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t count;
+	ssize_t count, countWrite;
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * letters);
@@ -22,8 +22,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);	
 	count = read(fd, buffer, letters);
-	write(STDOUT_FILENO, buffer, count);
+	countWrite = write(STDOUT_FILENO, buffer, count);
 	free(buffer);
-
-	return (count);
+	close(fd);
+	return (countWrite);
 }
