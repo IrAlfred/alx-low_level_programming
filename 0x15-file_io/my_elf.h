95b4f1532878906dbc060e5ff7fb25d62f5cd744
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 08:51:02 -0700

    Another test

--- 0x15-file_io/my_elf.h
+++ 0x15-file_io/my_elf.h
@@ -5,5 +5,14 @@
 #include <elf.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry_addr(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 #endif
