38c52633f502da967974ec20e4d0901f9b877754
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 04:09:27 -0700

    Changes to test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,18 +15,36 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(stderr, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	file_to = open(argv[1], O_WRONLY);
-
-
+	file_to = open(argv[2], O_WRONLY);
+	if (file_to == -1)
+		file_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
+	while (read(file_from, buffer, 1024) > 0)
+	{
+		if (write(file_to, buffer, (1024 / sizeof(char))) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
 }
