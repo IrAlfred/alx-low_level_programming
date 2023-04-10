637c4abca62cebc1eae7639dac3e154903a42811
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 07:52:55 -0700

    Add print elf squeleton

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of program
+ * @argc: number of args
+ * @argv: array or arguments
+ *
+ * Return: int (0 success)
+ */
+int main(int argc, **argv)
+{
+
+}
