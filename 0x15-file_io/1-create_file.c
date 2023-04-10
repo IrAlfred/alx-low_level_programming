50e07676017c71ac65ac4a8cf32a697f61319901
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 21:12:09 -0700

    Step one - some codes

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_file - Function that creates a file
+ * @filename: filename of file to be created
+ * @text_content: content to fill
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+}
