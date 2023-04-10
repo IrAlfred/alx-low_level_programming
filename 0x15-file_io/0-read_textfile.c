30b836eaf7fb760a51848e908788fa54040c2283
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 20:01:36 -0700

    Some codes to test read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,5 +9,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t count;
+	char buffer[letters];
 
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	
+	while (count = read(fd, buffer, letters) > 0)
+	{
+		if (write(STDOUT_FILENO, buffer, count) != count)
+		{
+			close(fd);
+			return (0);
+		}
+	}
 }
