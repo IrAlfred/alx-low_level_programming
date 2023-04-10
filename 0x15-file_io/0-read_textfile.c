e9da375ba77cb4ac120257ecb6a42227238c65a9
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 21:01:19 -0700

    Some charnges for betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 /**
  * read_textfile - Reads a text file and prints it to stdout
  * @filename: string filename
- * @letter: number of characters to read
+ * @letters: number of characters to read
  *
  * Return: the number of characters read
  */
@@ -16,11 +16,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+
 	if (filename == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);	
+		return (0);
+
 	count = read(fd, buffer, letters);
 	countWrite = write(STDOUT_FILENO, buffer, count);
 	free(buffer);
