831ed71891adbcfdc133d9484e2b2050ce611067
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 09:02:15 -0700

    another test

--- 0x15-file_io/my_elf.h
+++ 0x15-file_io/my_elf.h
@@ -15,4 +15,77 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry_addr(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
+
+/**
+ * check_elf - Checks if a file is the ELF file
+ * @e_ident: A pointer to an array containing the ELF magic numbers
+ *
+ * Return: nothing
+ */
+void check_elf(unsigned char *e_ident)
+{
+	int index;
+
+	for (index = 0; index < 4; index++)
+	{
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
+/**
+ * print_magic - Prints the magic numbers of an ELF header
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Return: nothing
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int index;
+
+	printf("  Magic:   ");
+
+	for (index = 0; index < EI_NIDENT; index++)
+	{
+		printf("%02x", e_ident[index]);
+
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+
+/**
+ * print_class - Prints the class of the ELF header
+ * @e_ident: A pointer to an array containing the ELF class
+ *
+ * Return: nothing
+ */
+void print_class(unsigned char *e_ident)
+{
+	printf("  Class:                             ");
+
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
 #endif
