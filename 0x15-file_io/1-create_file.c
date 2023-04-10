2d5bb42f93f812c332d61bc13f720a492985abaf
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 22:27:13 -0700

    Some changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-		if (fd == -1)
-			return (-1);
-		close(fd);
-		return (1);
+		text_content = "";
 	}
 
 	fd = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
