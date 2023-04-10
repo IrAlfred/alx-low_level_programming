8a6b720d83c01f982b7a7a34829ffc0e4d45aafc
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-09 19:33:19 -0700

    Add read_textfile prototype

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to stdout
+ * @filename: string filename
+ * @letter: number of characters to read
+ *
+ * Return: the number of characters read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
