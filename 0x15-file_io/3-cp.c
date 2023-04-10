713f765bab26db7ea5cd31474b692d47835d0d90
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 01:58:33 -0700

    Test if file_from exist

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,10 +10,18 @@
  */
 int main(int argc, char **argv)
 {
+	int file1, file2, nbChars;
+
 	if (argc != 3)
 	{
 		fprintf(stderr, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	return (0);
 }
