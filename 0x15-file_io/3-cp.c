a2b1a313d76edc3beaa52ed80cdb45534071f8b5
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 01:11:37 -0700

    Try verif number of args

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point of the program
@@ -9,5 +10,10 @@
  */
 int void main(int argc, char **argv)
 {
-	
+	if (argc != 3)
+	{
+		exit(97);
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+	}
+	return (0);
 }
