e869b43378fa15e539f7c57505ad2efcec7e2e2e
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 07:02:08 -0700

    some changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,10 +15,7 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
@@ -34,24 +31,18 @@ int main(int argc, char **argv)
 		nbCharsRead = read(file_from, buffer, 1024);
 		if (nbCharsRead == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		if (write(file_to, buffer, nbCharsRead) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
