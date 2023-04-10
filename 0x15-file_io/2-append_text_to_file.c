40994febfb5a5e65074d3fce1f4c754f926626e1
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-04-10 00:09:07 -0700

    Forth test

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	while (text_content[nbLetters])
 		nbLetters++;
+	nbLetters--;
 	if (write(fd, text_content, nbLetters) == -1)
 		return (-1);
+	close(fd);
 	return (1);
 }
