c216eb8e5b7ae4f762ada88fa0af176d0b0047d8
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-05-04 03:37:47 -0700

    Finished last test

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,37 @@
 #include "main.h"
+
+/**
+ * get_max - get the maximum number with a given number of bits
+ * @n: number of bits
+ *
+ * Return: number
+ */
+unsigned long int get_max(int n)
+{
+	int i;
+	unsigned long int max = 1;
+
+	for (i = 0; i < n; i++)
+	{
+		max *= 2;
+	}
+	return (max);
+}
+
+/**
+ * get_bit - Gets the value of a bit at a given index
+ * @n: number
+ * @index: index of the bit
+ *
+ * Return: Value of the bit or -1 on error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+	if (n & (1 << index))
+		return (1);
+	else
+		return (0);
+}
+
